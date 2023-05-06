59140ba0ec1748791b462717a46ee3bfcb2da5b3
Avando Campos <avandocampos@gmail.com>
2023-05-06 00:08:50 -0300

    Recoded all graph_gen scrip to permit subplot

--- graph_gen.py
+++ graph_gen.py
@@ -1,28 +1,49 @@
 import json
 import matplotlib.pyplot as plt
 
-# Lê o arquivo de resultados em formato JSON
+# Lê os dados do arquivo gerado pelo código anterior
 with open('resultados.json', 'r') as arquivo_json:
     resultados = json.load(arquivo_json)
 
-# Extrai as informações de tempo e taxa de transferência dos testes TCP e UDP
-tempo_tcp = []
-taxa_tcp = []
-for item in resultados['TCP']['intervals']:
-    tempo_tcp.append(item['sum']['start'])
-    taxa_tcp.append(item['sum']['bits_per_second'] / 1e9)
+# Extrai as taxas de transferência e os tempos de cada teste
+taxas_tcp_send = [float(x['sum']['bits_per_second'])/1e9 for x in resultados['TCP_SEND']['intervals']]
+tempos_tcp_send = [x['sum']['start'] for x in resultados['TCP_SEND']['intervals']]
 
-tempo_udp = []
-taxa_udp = []
-for item in resultados['UDP']['intervals']:
-    tempo_udp.append(item['sum']['start'])
-    taxa_udp.append(item['sum']['bits_per_second'] / 1e9)
+taxas_udp_send = [float(x['sum']['bits_per_second'])/1e9 for x in resultados['UDP_SEND']['intervals']]
+tempos_udp_send = [x['sum']['start'] for x in resultados['UDP_SEND']['intervals']]
 
-# Gera o gráfico de linha com os resultados
-plt.plot(tempo_tcp, taxa_tcp, label='TCP')
-plt.plot(tempo_udp, taxa_udp, label='UDP')
-plt.ylabel('Taxa de transferência (Gbps)')
-plt.xlabel('Tempo decorrido (s)')
-plt.title('Teste de velocidade de banda')
-plt.legend()
-plt.show()
+taxas_tcp_receive = [float(x['sum']['bits_per_second'])/1e9 for x in resultados['TCP_RECEIVE']['intervals']]
+tempos_tcp_receive = [x['sum']['start'] for x in resultados['TCP_RECEIVE']['intervals']]
+
+taxas_udp_receive = [float(x['sum']['bits_per_second'])/1e9 for x in resultados['UDP_RECEIVE']['intervals']]
+tempos_udp_receive = [x['sum']['start'] for x in resultados['UDP_RECEIVE']['intervals']]
+
+# Cria os subplots
+fig, (ax1, ax2) = plt.subplots(2, 1, figsize=(12, 8))
+
+# Ajusta o espaçamendo entre os gráficos
+fig.subplots_adjust(hspace=0.5)
+
+# Plota o primeiro gráfico
+ax1.plot(tempos_tcp_send, taxas_tcp_send, label='TCP Send')
+ax1.plot(tempos_udp_send, taxas_udp_send, label='UDP Send')
+ax1.set_title('Taxa de Transferência (Envio)')
+ax1.set_xlabel('Tempo (s)')
+ax1.set_ylabel('Taxa de Transferência (Gbps)')
+ax1.legend()
+ax1.grid()
+
+# Plota o segundo gráfico
+ax2.plot(tempos_tcp_receive, taxas_tcp_receive, label='TCP Receive')
+ax2.plot(tempos_udp_receive, taxas_udp_receive, label='UDP Receive')
+ax2.set_title('Taxa de Transferência (Recebimento)')
+ax2.set_xlabel('Tempo (s)')
+ax2.set_ylabel('Taxa de Transferência (Gbps)')
+ax2.legend()
+ax2.grid()
+
+# Exibe os gráficos
+# plt.show()
+
+# Salva o gráfico gerado em formato png
+plt.savefig('graph.png')
