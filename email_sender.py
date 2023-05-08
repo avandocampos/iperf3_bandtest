45f448ea944e4cea3a482aefcfb8bd23605f6b39
Avando Campos <avandocampos@gmail.com>
2023-05-08 15:46:02 -0300

    Decoupled email body info

--- email_sender.py
+++ email_sender.py
@@ -2,8 +2,10 @@ import smtplib
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 from email.mime.image import MIMEImage
+from email_text import html_format
 from decouple import config
 import base64
+import json
 
 # Informações do servidor SMTP e credenciais de login
 smtp_server = config('SMTP_SERVER', default='smtp.gmail.com')
@@ -13,18 +15,12 @@ smtp_password = config('SMTP_PASSWORD')
 
 # Destinatário e assunto do e-mail
 to_email = config('TO_EMAIL', default='avando.campos@sti.ufc.br')
-email_subject = 'Teste de e-mail com imagem embutida'
+email_subject = 'Gráficos dos testes de velocidade'
+email_body = html_format
 
-# Corpo do e-mail
-email_body = """
-<html>
-  <body>
-    <p>Olá,</p>
-    <p>Segue abaixo uma imagem embutida:</p>
-    <img src="cid:image1" width="1000">
-  </body>
-</html>
-"""
+# Lê os dados do arquivo gerado pelo código anterior
+with open('results.json', 'r') as json_file:
+    results = json.load(json_file)
 
 # Ler a imagem e converter para formato base64
 with open('graph.png', 'rb') as f:
@@ -39,7 +35,7 @@ msg.attach(MIMEText(email_body, 'html'))
 
 # Cria o objeto MIMEImage com a imagem em formato base64
 img = MIMEImage(base64.b64decode(img_base64))
-img.add_header('Content-ID', '<image1>')
+img.add_header('Content-ID', '<graph>')
 msg.attach(img)
 
 # Conecta-se ao servidor SMTP e envia o e-mail
