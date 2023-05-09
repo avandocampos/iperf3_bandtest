25d47cd49d96cf14578a02f0f226bddfb15fc81f
Avando Campos <avandocampos@gmail.com>
2023-05-09 11:49:47 -0300

    refactoring and adding more data

--- email_text.py
+++ email_text.py
@@ -1,5 +1,36 @@
 def to_html(results):
 
+    style = """
+        <style>
+            .table-container {
+                display: flex;
+                flex-direction: row;
+                width: 100%;
+                margin-top: 20px;
+            }
+
+            .table-container div {
+                flex: 1;
+            }
+
+            table {
+                border-collapse: collapse;
+                margin: 0 10px;
+            }
+
+            th, td {
+                padding: 8px;
+                text-align: left;
+                border: 1px solid #ddd;
+            }
+
+            th {
+                background-color: #f2f2f2;
+                color: #555;
+            }
+        </style>
+    """
+
     email_body = f""" <!DOCTYPE html>
     <html lang="pt-br">
     <head>
@@ -7,29 +38,119 @@ def to_html(results):
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Gráficos</title>
+
+        {style}
+
     </head>
     <body>
-        <table border="1px solid" cellspacing="0.1" border-collapse="collapse">
-            <thead>
-                <tr>
-                    <th colspan="3">ENVIO DE DADOS TCP</th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr>
-                    <th>Bytes enviados</th>
-                    <th>Tempo total (s)</th>
-                    <th>Bits por segundo (média)</th>
-                </tr>
-                <tr>
-                    <td>{results['TCP_SEND']['end']['streams'][0]['sender']['bytes']}</td>
-                    <td>{results['TCP_SEND']['end']['streams'][0]['sender']['seconds']}</td>
-                    <td>{results['TCP_SEND']['end']['streams'][0]['sender']['bits_per_second']}</td>
-                </tr>
-            </tbody>
-    </table>
+        <div class="table-container">
+            <table id="tcp-send">
+                <thead>
+                    <tr>
+                        <th colspan="4">ENVIO DE DADOS TCP</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <th>Bytes enviados</th>
+                        <th>Tempo total (s)</th>
+                        <th>Bits por segundo (média)</th>
+                        <th>Reenvios</th>
+                    </tr>
+                    <tr>
+                        <td>{results['TCP_SEND']['end']['streams'][0]['sender']['bytes']}</td>
+                        <td>{results['TCP_SEND']['end']['streams'][0]['sender']['seconds']}</td>
+                        <td>{results['TCP_SEND']['end']['streams'][0]['sender']['bits_per_second']}</td>
+                        <td>{results['TCP_SEND']['end']['streams'][0]['sender']['retransmits']}</td>
+                    </tr>
+                </tbody>
+            </table>
 
-        <img src="cid:graph" width="1000">
+            <table id="tcp-receive">
+                <thead>
+                    <tr>
+                        <th colspan="4">RECEBIMENTO DE DADOS TCP</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <th>Bytes Recebidos</th>
+                        <th>Tempo total (s)</th>
+                        <th>Bits por segundo (média)</th>
+                        <th>Reenvios</th>
+                    </tr>
+                    <tr>
+                        <td>{results['TCP_RECEIVE']['end']['streams'][0]['sender']['bytes']}</td>
+                        <td>{results['TCP_RECEIVE']['end']['streams'][0]['sender']['seconds']}</td>
+                        <td>{results['TCP_RECEIVE']['end']['streams'][0]['sender']['bits_per_second']}</td>
+                        <td>{results['TCP_RECEIVE']['end']['streams'][0]['sender']['retransmits']}</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+
+        <div class="table-container">
+            <table id="udp-send">
+                <thead>
+                    <tr>
+                        <th colspan="7">ENVIO DE DADOS UDP</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <th>Bytes Enviados</th>
+                        <th>Tempo total (s)</th>
+                        <th>Bits por segundo (média)</th>
+                        <th>Jitter (ms)</th>
+                        <th>Lost packets</th>
+                        <th>Packets</th>
+                        <th>Lost percent</th>
+                    </tr>
+                    <tr>
+                        <td>{results['UDP_SEND']['end']['sum']['bytes']}</td>
+                        <td>{results['UDP_SEND']['end']['sum']['seconds']}</td>
+                        <td>{results['UDP_SEND']['end']['sum']['bits_per_second']}</td>
+                        <td>{results['UDP_SEND']['end']['sum']['jitter_ms']}</td>
+                        <td>{results['UDP_SEND']['end']['sum']['lost_packets']}</td>
+                        <td>{results['UDP_SEND']['end']['sum']['packets']}</td>
+                        <td>{results['UDP_SEND']['end']['sum']['lost_percent']}</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+        
+        <div class="table-container">
+            <table id="udp-receive">
+                <thead>
+                    <tr>
+                        <th colspan="7">RECEBIMENTO DE DADOS UDP</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <th>Bytes Recebidos</th>
+                        <th>Tempo total (s)</th>
+                        <th>Bits por segundo (média)</th>
+                        <th>Jitter (ms)</th>
+                        <th>Lost packets</th>
+                        <th>Packets</th>
+                        <th>Lost percent</th>
+                    </tr>
+                    <tr>
+                        <td>{results['UDP_RECEIVE']['end']['sum']['bytes']}</td>
+                        <td>{results['UDP_RECEIVE']['end']['sum']['seconds']}</td>
+                        <td>{results['UDP_RECEIVE']['end']['sum']['bits_per_second']}</td>
+                        <td>{results['UDP_RECEIVE']['end']['sum']['jitter_ms']}</td>
+                        <td>{results['UDP_RECEIVE']['end']['sum']['lost_packets']}</td>
+                        <td>{results['UDP_RECEIVE']['end']['sum']['packets']}</td>
+                        <td>{results['UDP_RECEIVE']['end']['sum']['lost_percent']}</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+
+        <img src="cid:graph" width="1200">
+
     </body>
     </html> """
 
