83edaa1c7aded7c35e2d071b66c6cd9ad4a71ab0
Avando Campos <avandocampos@gmail.com>
2023-05-08 22:24:09 -0300

    added plot_graph function call

--- client_iperf3.py
+++ client_iperf3.py
@@ -16,7 +16,7 @@ time = config('TIME', default=60, cast=int)
 time_wait = config('TIME_WAIT', default=60, cast=int)
 
 # Realiza o teste TCP enviando dados para o servidor
-print("Enviando dados TCP...")
+print("Seding TCP data...")
 tcp_send_output = subprocess.check_output(f'iperf3 -c {remote_host} -p {port} -t {time} -i 1 --json', shell=True)
 
 # Converte a saída em um objeto JSON
@@ -26,7 +26,7 @@ tcp_send_result = json.loads(tcp_send_output.decode())
 sleep(time_wait)
 
 # Realiza o teste UDP enviando dados para o servidor
-print("Enviando dados UDP...")
+print("Sending UDP data...")
 udp_send_output = subprocess.check_output(f'iperf3 -c {remote_host} -p {port} -t {time} -i 1 -u -b 0 --json', shell=True)
 
 # Converte a saída em um objeto JSON
@@ -35,7 +35,7 @@ udp_send_result = json.loads(udp_send_output.decode())
 sleep(time_wait)
 
 # Realiza o teste TCP recebendo dados do servidor
-print("Recebendo dados TCP...")
+print("Receiving TCP data...")
 tcp_receive_output = subprocess.check_output(f'iperf3 -c {remote_host} -p {port} -t {time} -i 1 --reverse --json', shell=True)
 
 # Converte a saída em um objeto JSON
@@ -44,7 +44,7 @@ tcp_receive_result = json.loads(tcp_receive_output.decode())
 sleep(time_wait)
 
 # Realiza o teste UDP recebendo dados do servidor
-print("Recebendo dados UDP...")
+print("Receiving UDP data...")
 udp_receive_output = subprocess.check_output(f'iperf3 -c {remote_host} -p {port} -t {time} -i 1 -u -b 0 --reverse --json', shell=True)
 
 # Converte a saída em um objeto JSON
@@ -60,3 +60,16 @@ with open('results.json', 'w') as json_file:
             'UDP_RECEIVE': udp_receive_result
         }, json_file, indent=4
     )
+
+# Verifica se deve criar um gráfico com os resultados
+graph = config('PLOT_GRAPH', default=False, cast=bool)
+
+if graph:
+    from graph_gen import plot_graph
+
+    with open('results.json', 'r') as json_file:
+        results = json.load(json_file)
+
+    print('Ploting graph...')
+
+    plot_graph(results)
